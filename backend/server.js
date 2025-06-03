8fba997599fc3ac751c14b78781677ac9c64952b
Cheems123-stack <khoi.nguyen2252379@hcmut.edu.vn>
2025-06-03 09:41:14 +0700

    backend complete

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ import { v2 as cloudinary } from 'cloudinary';
 import authRoutes from './routes/auth.route.js';
 import userRoutes from './routes/user.route.js';
 import postRoutes from './routes/post.route.js';
-
+import notificationRoutes from './routes/notification.route.js';
 
 import connectMongoDB from './db/connectMongoDB.js';
 
@@ -32,6 +32,8 @@ app.use("/api/auth",authRoutes);
 app.use("/api/users",userRoutes);
 
 app.use("/api/posts",postRoutes);
+
+app.use("/api/notifications", notificationRoutes);
 
 app.listen(PORT, () => {
   connectMongoDB();
